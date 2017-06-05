5634931e14c40f0b999cb4308092a6eead5ba798
LipaiValentine <mr.val@mail.ru>
2017-06-06 02:38:36 +0300

    update

--- js/common.js
+++ js/common.js
@@ -53,7 +53,7 @@ $('#form').validate({
 			minlength: 3,
 			email: true
 		},
-		password:{
+		phone:{
 			required: true,
 			minlength: 6
 		}
@@ -65,11 +65,66 @@ $('#form').validate({
 		},
 		email:{
 			required: 'Это обязательное поле',
-			minlength: 'Не менее 3 символов'
+			minlength: 'Не менее 3 символов',
+			email: 'Введите правильный e-mail'
 		},
-		password:{				
+		phone:{				
 			required: 'Это обязательное поле',
 			minlength: 'Не менее 6 символов'
 		}
 	}
 });
+
+		$(document).ready(function(){
+			var showButton = $('.show-items');
+			var itemsBoxHeight = $('.results__item-box');
+			var link =$('.menu-link');
+			var link_active= $('.menu-link__active');
+			var menu= $('.menu');
+			var menu_active= $('.menu-active');
+			var hide_menu= $('.menu a');
+			var searchInput = $('.header__search a');
+
+			link.click(function(){
+				link.toggleClass('menu-link__active');
+				menu.toggleClass('menu-active');
+			});
+			hide_menu.click(function(){
+				link.toggleClass('menu-link__active');
+				menu.toggleClass('menu-active');
+			});
+
+			showButton.click(function(){
+				var target = $(this).attr('target');
+				$(this).parents('.results__item-box').addClass('new-height-item-box');
+				$(target).toggle(50);
+				$(this).remove();
+			});
+
+			searchInput.click(function(){
+				$('.header__search input').toggleClass('hidden');
+			});
+			$(".menu-scroll").on("click","a", function (event) {
+					//отменяем стандартную обработку нажатия по ссылке
+					event.preventDefault();
+
+					//забираем идентификатор бока с атрибута href
+					var id  = $(this).attr('href'),
+
+					//узнаем высоту от начала страницы до блока на который ссылается якорь
+					top = $(id).offset().top;
+					
+					//анимируем переход на расстояние - top за 1500 мс
+					$('body,html').animate({scrollTop: top}, 1500);
+				});
+		});
+
+		$('#myTabs a').click(function (e) {
+			e.preventDefault()
+			$(this).tab('show')
+		});
+
+		$('.show-more-btn').click(function(){
+			$('.results__items__show-more').show(1000);
+			$(this).remove();
+		});
