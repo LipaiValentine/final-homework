1789ae8208ff66a36518ef9dcbcb63d3dccae1da
LipaiValentine <mr.val@mail.ru>
2017-06-05 12:22:45 +0300

    update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,47 @@
+$("#use-systems-logo-gellery").owlCarousel({
+				items:10,
+				loop: true,
+				autoplay: true,
+				smartSpeed: 1000,
+				autoplayTimeout: 2000,
+				responsive:{
+					0:{
+						items:3
+					},
+					500:{
+						items:5
+					},
+					1000:{
+						items:8
+					}
+				}
+});
+$("#our-partners-logo-gellery").owlCarousel({
+	items: 5,
+	loop: true,
+	autoplay: true,
+	smartSpeed: 500,
+	autoplayTimeout: 3000,
+	margin: 50,
+	responsive:{
+		0:{
+			items:3,
+			margin: 10
+		},
+		1000:{
+			items:5,
+			margin: 10
+		}
+	}
+});
+$(".bd-desc-tabs__bottom-logos-mobile").owlCarousel({
+	items: 3,
+	loop: true,
+	autoplay: true,
+	smartSpeed: 500,
+	autoplayTimeout: 3000,
+	margin: 20
+});
 $('#form').validate({
 	rules:{
 		name:{
